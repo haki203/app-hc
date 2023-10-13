3a1cc296d2ef2fa1671a8a121bc7fa78dd327107
NguyenLongVu03 <117622104+NguyenLongVu03@users.noreply.github.com>
2023-10-13 17:31:57 +0700

    history

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Report from './src/LongVu/Report'
+import History from './src/LongVu/History'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Report/>
+      <History/>
     </View>
   )
 }
