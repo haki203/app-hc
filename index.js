7162b20a66c7a65de38898c5097d09566a8af8fd
tuananh2k321 <haizzj123@gmail.com>
2023-10-13 11:54:33 +0700

    TA done task

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import SupportForm from './SupportForm';
+import ReportProblem from './ReportProblem';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ReportProblem);
