4613d688455bd12b0a6104e78be6d6d2bf491212
theking250902 <buivanvietx5hongloc@gmail.com>
2023-10-19 21:16:36 +0700

    Revert "done"
    This reverts commit d8c35c1534663bfa82ac215a13ea4afd39d09003.

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { Image, Pressable, StyleSheet, Text, View, ToastAndroid } from 'react-na
 import React, { useContext } from 'react'
 const baseImgPath = '../assets/images/';
 import { AppContext } from '../context/AppContext';
-import { _siginWithGoogle } from '../firebase/GoogleSigin';
+
 import AxiosIntance from '../axios/AxiosIntance';
 const Login = () => {
   const { setinfoUser, setIsLogin } = useContext(AppContext);
@@ -12,7 +12,6 @@ const Login = () => {
   const logingoogle = async () => {
     try {
       console.log("login");
-      _siginWithGoogle();
       const res = await AxiosIntance().get("/user/login-google/tho387vm@gmail.com ");
       setinfoUser(res.user);
       console.log(res.user);
@@ -20,8 +19,8 @@ const Login = () => {
       setIsLogin(true);
 
     }
-    catch (error) {
-      ToastAndroid.show("Đăng nhập thất bại" + error, ToastAndroid.SHORT);
+    catch (error){
+      ToastAndroid.show("Đăng nhập thất bại"+error, ToastAndroid.SHORT);
     }
   }
   return (
