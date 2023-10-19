f07d68a3de3c8db8ed4ae8c1330b1507e88c5a33
theking250902 <buivanvietx5hongloc@gmail.com>
2023-10-19 21:28:07 +0700

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,22 @@ import { Image, Pressable, StyleSheet, Text, View, ToastAndroid } from 'react-na
 import React, { useContext } from 'react'
 const baseImgPath = '../assets/images/';
 import { AppContext } from '../context/AppContext';
-
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 import AxiosIntance from '../axios/AxiosIntance';
 const Login = () => {
   const { setinfoUser, setIsLogin } = useContext(AppContext);
 
-
+  GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+    webClientId: '796405893611-6ka7dp7je723lkc54igmi0u43suo76vk.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+    offlineAccess: false, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  });
 
   const logingoogle = async () => {
     try {
-      console.log("login");
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
       const res = await AxiosIntance().get("/user/login-google/tho387vm@gmail.com ");
       setinfoUser(res.user);
       console.log(res.user);
@@ -19,8 +25,8 @@ const Login = () => {
       setIsLogin(true);
 
     }
-    catch (error){
-      ToastAndroid.show("Đăng nhập thất bại"+error, ToastAndroid.SHORT);
+    catch (error) {
+      ToastAndroid.show("Đăng nhập thất bại" + error, ToastAndroid.SHORT);
     }
   }
   return (
