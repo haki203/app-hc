8c37668a54b3a2f1853d6084415b6ab6e00a25b5
NguyenLongVu03 <117622104+NguyenLongVu03@users.noreply.github.com>
2023-10-25 18:05:25 +0700

    .admin

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import React from 'react'
 import { AppContextProvider } from './src/context/AppContext'
 import { NavigationContainer } from '@react-navigation/native'
 import AppNavigator from './src/navigation/AppNavigator'
+import HistoryAdmin from './src/components/admin/HistoryAdmin'
+import ReportAdmin from './src/components/admin/ReportAdmin'
+import SupportForm from './src/components/user/SupportForm'
+import HandlReports from './src/components/admin/HandlReports'
 
 const App = () => {
   return (
