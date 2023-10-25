e4fbff2267dbc6a1b205e59015a1eceec6c19db2
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-10-25 23:20:51 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,13 +4,7 @@ import React from 'react'
 import { AppContextProvider } from './src/context/AppContext'
 import { NavigationContainer } from '@react-navigation/native'
 import AppNavigator from './src/navigation/AppNavigator'
-<<<<<<< HEAD
-=======
-import HistoryAdmin from './src/components/admin/HistoryAdmin'
-import ReportAdmin from './src/components/admin/ReportAdmin'
-import SupportForm from './src/components/user/SupportForm'
-import HandlReports from './src/components/admin/HandlReports'
->>>>>>> parent of 3d701eb (Merge branch 'test' into chien_@@)
+
 
 const App = () => {
   return (
