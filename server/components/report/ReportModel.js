d053518f90f42b0f6b6fd44b2e9d0e4d4b3f3436
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-11-12 20:53:48 +0700

    Update ReportModel.js

--- server/components/report/ReportModel.js
+++ server/components/report/ReportModel.js
@@ -11,6 +11,7 @@ const schema = new Schema({
     time: { type: String },
     admin: {  type: ObjectId, ref: 'admins'  },
     accept: { type: String },
+    comment: { type: String ,default:""},
     done: { type: String },
     description: { type: String },
     status: { type: Number },
